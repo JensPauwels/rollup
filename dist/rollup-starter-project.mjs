dafa92ef6d439c4219b6c8b1180fa02ffb79ed89
Jens Pauwels <jens@dptechnics.com>
2018-12-02 22:50:12 +0100

    updated

---
+++ dist/rollup-starter-project.mjs
@@ -0,0 +1,34 @@
+var classCallCheck = function (instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+};
+
+var __cov_so9ih2bl3SyzO0vmkBRwxQ = (Function('return this'))();
+if (!__cov_so9ih2bl3SyzO0vmkBRwxQ.__coverage__) { __cov_so9ih2bl3SyzO0vmkBRwxQ.__coverage__ = {}; }
+__cov_so9ih2bl3SyzO0vmkBRwxQ = __cov_so9ih2bl3SyzO0vmkBRwxQ.__coverage__;
+if (!(__cov_so9ih2bl3SyzO0vmkBRwxQ['/home/jens/Documents/dev/library/lib/Test.js'])) {
+   __cov_so9ih2bl3SyzO0vmkBRwxQ['/home/jens/Documents/dev/library/lib/Test.js'] = {"path":"/home/jens/Documents/dev/library/lib/Test.js","s":{"1":0,"2":0,"3":0},"b":{},"f":{"1":0},"fnMap":{"1":{"name":"Test","line":1,"loc":{"start":{"line":1,"column":11},"end":{"line":1,"column":31}}}},"statementMap":{"1":{"start":{"line":1,"column":0},"end":{"line":5,"column":2}},"2":{"start":{"line":2,"column":2},"end":{"line":2,"column":42}},"3":{"start":{"line":4,"column":2},"end":{"line":4,"column":19}}},"branchMap":{}};
+}
+__cov_so9ih2bl3SyzO0vmkBRwxQ = __cov_so9ih2bl3SyzO0vmkBRwxQ['/home/jens/Documents/dev/library/lib/Test.js'];
+__cov_so9ih2bl3SyzO0vmkBRwxQ.s['1']++;var Test=function Test(data){__cov_so9ih2bl3SyzO0vmkBRwxQ.f['1']++;__cov_so9ih2bl3SyzO0vmkBRwxQ.s['2']++;classCallCheck(this,Test);__cov_so9ih2bl3SyzO0vmkBRwxQ.s['3']++;this.data=data;};
+
+var __cov__s95GdoT$5aUyPz2yKUdBA = (Function('return this'))();
+if (!__cov__s95GdoT$5aUyPz2yKUdBA.__coverage__) { __cov__s95GdoT$5aUyPz2yKUdBA.__coverage__ = {}; }
+__cov__s95GdoT$5aUyPz2yKUdBA = __cov__s95GdoT$5aUyPz2yKUdBA.__coverage__;
+if (!(__cov__s95GdoT$5aUyPz2yKUdBA['/home/jens/Documents/dev/library/lib/Another.js'])) {
+   __cov__s95GdoT$5aUyPz2yKUdBA['/home/jens/Documents/dev/library/lib/Another.js'] = {"path":"/home/jens/Documents/dev/library/lib/Another.js","s":{"1":0,"2":0,"3":0},"b":{},"f":{"1":0},"fnMap":{"1":{"name":"Another","line":1,"loc":{"start":{"line":1,"column":14},"end":{"line":1,"column":37}}}},"statementMap":{"1":{"start":{"line":1,"column":0},"end":{"line":5,"column":2}},"2":{"start":{"line":2,"column":2},"end":{"line":2,"column":45}},"3":{"start":{"line":4,"column":2},"end":{"line":4,"column":19}}},"branchMap":{}};
+}
+__cov__s95GdoT$5aUyPz2yKUdBA = __cov__s95GdoT$5aUyPz2yKUdBA['/home/jens/Documents/dev/library/lib/Another.js'];
+__cov__s95GdoT$5aUyPz2yKUdBA.s['1']++;var Another=function Another(data){__cov__s95GdoT$5aUyPz2yKUdBA.f['1']++;__cov__s95GdoT$5aUyPz2yKUdBA.s['2']++;classCallCheck(this,Another);__cov__s95GdoT$5aUyPz2yKUdBA.s['3']++;this.data=data;};
+
+var __cov_zzI9r28xDULATjnTkC7e5A = (Function('return this'))();
+if (!__cov_zzI9r28xDULATjnTkC7e5A.__coverage__) { __cov_zzI9r28xDULATjnTkC7e5A.__coverage__ = {}; }
+__cov_zzI9r28xDULATjnTkC7e5A = __cov_zzI9r28xDULATjnTkC7e5A.__coverage__;
+if (!(__cov_zzI9r28xDULATjnTkC7e5A['/home/jens/Documents/dev/library/lib/index.js'])) {
+   __cov_zzI9r28xDULATjnTkC7e5A['/home/jens/Documents/dev/library/lib/index.js'] = {"path":"/home/jens/Documents/dev/library/lib/index.js","s":{},"b":{},"f":{},"fnMap":{},"statementMap":{},"branchMap":{}};
+}
+__cov_zzI9r28xDULATjnTkC7e5A = __cov_zzI9r28xDULATjnTkC7e5A['/home/jens/Documents/dev/library/lib/index.js'];
+
+export { Test, Another };
+//# sourceMappingURL=rollup-starter-project.mjs.map
--- rollup.config.js
+++ rollup.config.js
@@ -23,7 +23,7 @@ export default {
     {
       dest: pkg.main,
       format: 'umd',
-      moduleName: 'rollupStarterProject',
+      moduleName: 'library',
       sourceMap: true
     },
     {
